7e9ec409e6789d4cbb5f6edcd6d5dafee2fdcdc3
Fernando <lfgc1@softtek.com>
2020-02-18 12:18:53 -0600

    youtube + service wcf

---
+++ Youtube v2.0 + service/RockolaYoutubeProject/RockolaYoutubeProject/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Google.Apis.Services;
+using Google.Apis.YouTube.v3;
+
+namespace RockolaYoutubeProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // Se remplaza por el servici WCF
+
+
+        //[HttpGet]
+        //public ActionResult SearchList(string palabra)
+        //{
+        //    var youtubeService = new YouTubeService(new BaseClientService.Initializer()
+        //    {
+        //        ApiKey = "AIzaSyDLl03D6wFfhOaeqUIJ5wR2mDhXNckgAas",
+        //        ApplicationName = this.GetType().ToString()
+        //    });
+
+        //    var searchListRequest = youtubeService.Search.List("snippet");
+        //    searchListRequest.Q = palabra; // Replace with your search term.
+        //    searchListRequest.MaxResults = 5;
+
+        //    var searchListResponse = searchListRequest.Execute();
+
+        //    return PartialView("Search",searchListResponse.Items);
+        //}
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Youtube v2.0 + service/WCFProject/WCFProject/Service1.svc.cs
@@ -0,0 +1,56 @@
+﻿using Google.Apis.Services;
+using Google.Apis.YouTube.v3;
+using Google.Apis.YouTube.v3.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WCFProject
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+
+        public List<SearchResult> GetListYouTube (string keyword)
+        {
+            var youtubeService = new YouTubeService(new BaseClientService.Initializer()
+            {
+                ApiKey = "AIzaSyDLl03D6wFfhOaeqUIJ5wR2mDhXNckgAas",
+                //ApplicationName = this.GetType().ToString()
+            });
+
+            var searchListRequest = youtubeService.Search.List("snippet");
+            searchListRequest.Q = keyword; // Replace with your search term.
+            searchListRequest.MaxResults = 5;
+
+            var searchListResponse = searchListRequest.Execute();
+            IList<SearchResult> searchResults = searchListResponse.Items;
+
+            return searchResults.ToList(); 
+                //PartialView("Search", searchListResponse.Items);
+        }
+
+    }
+}
